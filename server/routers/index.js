3d66cabf0cb99567aec155ad0288451833608aa7
Hemnarayan <vy2756943@gmail.in>
2024-06-25 15:43:12 +0530

    n

--- server/routers/index.js
+++ server/routers/index.js
@@ -2,6 +2,8 @@ import express from 'express'
 
 
 const router = express.Router();
+
+//................................................................
 import { ForgetPassword, userLogin, userRegister, ResetPassword, profile } from '../controllers/user.controllers.js';
 
 router.post("/register", userRegister)
@@ -11,29 +13,23 @@ router.post("/reset_password/:id/:token",ResetPassword)
 router.get('/profile', profile);
 
 
-
+//................................................................
 import { getAllUsers } from '../controllers/admin.controller.js';
 import {auth, authorize} from '../middleware/authToken.js'
 
 router.get('/Allusers',auth, authorize(['admin']), getAllUsers);
 
 //.........................................................................................................
-
 import { addProd, getProd, editProd, deleteProd } from '../controllers/product.controller.js';
 import { upload } from '../middleware/multer.js';
 
 router.post('/addProd', upload.single('file'),addProd);
-
 router.get('/getProd', getProd);
-
 router.put('/editProd/:id', upload.single('file'),editProd);
-
 router.delete('/deleteProd/:id', deleteProd);
 
 //...................................................................................................
 import { addToCart, getCart, removeFromCart, updateCartItem } from '../controllers/cart.controller.js';
-
-
 
 router.post('/cart', auth, addToCart);
 router.get('/cart', auth, getCart);
